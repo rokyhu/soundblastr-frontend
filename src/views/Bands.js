ea41f743c67fd662017a02ceaa0bd6dfa7c89cdd
Viktor Dome <v.dome1991@gmail.com>
2021-09-23 12:23:39 +0200

    [add] bands component

---
+++ src/views/Bands.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Row,
+    Col,
+  } from "reactstrap";
+
+const Bands = (props) => {
+    return (
+        <div className="content">
+            <div class="d-flex flex-wrap">
+            <Card className="card-user max30w margin-after">
+              <div className="image">
+                <img
+                  alt="..."
+                  src={require("assets/img/damir-bosnjak.jpg").default}
+                />
+              </div>
+              <CardBody>
+                <div className="author">
+                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                    <img
+                      alt="..."
+                      className="avatar border-gray"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h5 className="title">Chet Faker</h5>
+                  </a>
+                  <p className="description">@chetfaker</p>
+                </div>
+                <p className="description text-center">
+                  "I like the way you work it <br />
+                  No diggity <br />I wanna bag it up"
+                </p>
+              </CardBody>
+              <CardFooter>
+                <hr />
+                <div className="button-container">
+                  <Row>
+                    <Col className="ml-auto" lg="3" md="6" xs="6">
+                      <h5>
+                        12 <br />
+                        <small>Files</small>
+                      </h5>
+                    </Col>
+                    <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                      <h5>
+                        2GB <br />
+                        <small>Used</small>
+                      </h5>
+                    </Col>
+                    <Col className="mr-auto" lg="3">
+                      <h5>
+                        24,6$ <br />
+                        <small>Spent</small>
+                      </h5>
+                    </Col>
+                  </Row>
+                </div>
+              </CardFooter>
+            </Card>
+            <Card className="card-user max30w margin-after">
+              <div className="image">
+                <img
+                  alt="..."
+                  src={require("assets/img/damir-bosnjak.jpg").default}
+                />
+              </div>
+              <CardBody>
+                <div className="author">
+                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                    <img
+                      alt="..."
+                      className="avatar border-gray"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h5 className="title">Chet Faker</h5>
+                  </a>
+                  <p className="description">@chetfaker</p>
+                </div>
+                <p className="description text-center">
+                  "I like the way you work it <br />
+                  No diggity <br />I wanna bag it up"
+                </p>
+              </CardBody>
+              <CardFooter>
+                <hr />
+                <div className="button-container">
+                  <Row>
+                    <Col className="ml-auto" lg="3" md="6" xs="6">
+                      <h5>
+                        12 <br />
+                        <small>Files</small>
+                      </h5>
+                    </Col>
+                    <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                      <h5>
+                        2GB <br />
+                        <small>Used</small>
+                      </h5>
+                    </Col>
+                    <Col className="mr-auto" lg="3">
+                      <h5>
+                        24,6$ <br />
+                        <small>Spent</small>
+                      </h5>
+                    </Col>
+                  </Row>
+                </div>
+              </CardFooter>
+            </Card>
+            <Card className="card-user max30w margin-after">
+              <div className="image">
+                <img
+                  alt="..."
+                  src={require("assets/img/damir-bosnjak.jpg").default}
+                />
+              </div>
+              <CardBody>
+                <div className="author">
+                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                    <img
+                      alt="..."
+                      className="avatar border-gray"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h5 className="title">Chet Faker</h5>
+                  </a>
+                  <p className="description">@chetfaker</p>
+                </div>
+                <p className="description text-center">
+                  "I like the way you work it <br />
+                  No diggity <br />I wanna bag it up"
+                </p>
+              </CardBody>
+              <CardFooter>
+                <hr />
+                <div className="button-container">
+                  <Row>
+                    <Col className="ml-auto" lg="3" md="6" xs="6">
+                      <h5>
+                        12 <br />
+                        <small>Files</small>
+                      </h5>
+                    </Col>
+                    <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                      <h5>
+                        2GB <br />
+                        <small>Used</small>
+                      </h5>
+                    </Col>
+                    <Col className="mr-auto" lg="3">
+                      <h5>
+                        24,6$ <br />
+                        <small>Spent</small>
+                      </h5>
+                    </Col>
+                  </Row>
+                </div>
+              </CardFooter>
+            </Card>
+            <Card className="card-user max30w margin-after">
+              <div className="image">
+                <img
+                  alt="..."
+                  src={require("assets/img/damir-bosnjak.jpg").default}
+                />
+              </div>
+              <CardBody>
+                <div className="author">
+                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                    <img
+                      alt="..."
+                      className="avatar border-gray"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h5 className="title">Chet Faker</h5>
+                  </a>
+                  <p className="description">@chetfaker</p>
+                </div>
+                <p className="description text-center">
+                  "I like the way you work it <br />
+                  No diggity <br />I wanna bag it up"
+                </p>
+              </CardBody>
+              <CardFooter>
+                <hr />
+                <div className="button-container">
+                  <Row>
+                    <Col className="ml-auto" lg="3" md="6" xs="6">
+                      <h5>
+                        12 <br />
+                        <small>Files</small>
+                      </h5>
+                    </Col>
+                    <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                      <h5>
+                        2GB <br />
+                        <small>Used</small>
+                      </h5>
+                    </Col>
+                    <Col className="mr-auto" lg="3">
+                      <h5>
+                        24,6$ <br />
+                        <small>Spent</small>
+                      </h5>
+                    </Col>
+                  </Row>
+                </div>
+              </CardFooter>
+            </Card>
+            </div>
+        </div>
+    )
+}
+
+export default Bands
