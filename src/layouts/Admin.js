7a2cd4032801edf6ea9be13d289235350bdcc036
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-24 01:27:08 +0200

    [change] font and sidebar size

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -22,7 +22,7 @@ import PerfectScrollbar from "perfect-scrollbar";
 import { Route, Switch, useLocation } from "react-router-dom";
 
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import Footer from "components/Footer/Footer.js";
+// import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
@@ -32,7 +32,7 @@ var ps;
 
 function Dashboard(props) {
   const [backgroundColor, setBackgroundColor] = React.useState("black");
-  const [activeColor, setActiveColor] = React.useState("info");
+  const [activeColor, setActiveColor] = React.useState("warning");
   const mainPanel = React.useRef();
   const location = useLocation();
   React.useEffect(() => {
@@ -78,7 +78,7 @@ function Dashboard(props) {
             );
           })}
         </Switch>
-        <Footer fluid />
+        {/* <Footer fluid /> */}
       </div>
       <FixedPlugin
         bgColor={backgroundColor}
