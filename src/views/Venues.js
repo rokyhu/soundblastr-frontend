bb4cc85cfc7efe880b54142a1628abe59aa381bd
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-24 02:10:17 +0200

    [remove] link to Venue details page

--- src/views/Venues.js
+++ src/views/Venues.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Venue from './Venue';
-import axios from 'axios';
-import VenueDetail from './VenueDetail';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import Venue from "./Venue";
+import axios from "axios";
+import VenueDetail from "./VenueDetail";
 
 const Venues = (props) => {
-    const [venues, setVenues] = useState([]);
-    const [error, setError] = useState('');
-    const [componentId, setComponentId] = useState('');
-    const url = "http://localhost:8080/venue/all";
+  const [venues, setVenues] = useState([]);
+  const [error, setError] = useState("");
+  const [componentId] = useState("");
+  const url = "http://localhost:8080/venue/all";
 
-    useEffect(() => {
-        axios
-            .get(url)
-            .then((res) => {
-                setVenues(res.data);
-            })
-            .catch((err) => {
-              setError(err.message)
-            })
-        }, []);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setVenues(res.data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
 
-    const handleClick = (id) => {
-        setComponentId(id);
-    } 
+  const handleClick = (id) => {
+    // setComponentId(id);
+  };
 
-    return (
-        <>
-        {componentId === '' ? (
+  return (
+    <>
+      {componentId === "" ? (
         <div className="content">
-            {error ? (
+          {error ? (
             <div>
-              An error occured while fetching the requested information. Please try
-              again!
+              An error occured while fetching the requested information. Please
+              try again!
             </div>
           ) : (
             <div className="d-flex flex-wrap">
-            {venues.map((venue) => (
-                    <Venue
-                    key={venue.id}
-                    id={venue.id}
-                    image={venue.imageUrl}
-                    name={venue.name}
-                    address={venue.address}
-                    description={venue.description}
-                    capacity={venue.capacity}
-                    onClick={handleClick}
-                    />
-            ))}
+              {venues.map((venue) => (
+                <Venue
+                  key={venue.id}
+                  id={venue.id}
+                  image={venue.imageUrl}
+                  name={venue.name}
+                  address={venue.address}
+                  description={venue.description}
+                  capacity={venue.capacity}
+                  onClick={handleClick}
+                />
+              ))}
             </div>
-        )}
+          )}
         </div>
-        ):(
-            <VenueDetail id={componentId} />
-        )}
-        </>
-    )
-}
-export default Venues
+      ) : (
+        <VenueDetail id={componentId} />
+      )}
+    </>
+  );
+};
+export default Venues;
