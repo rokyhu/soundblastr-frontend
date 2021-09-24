a5eddef509d2bfb5e5c0043e0ac8371fee11d5c7
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-24 02:09:05 +0200

    [add] Band title

--- src/views/Band.js
+++ src/views/Band.js
@@ -1,47 +1,36 @@
-import React from 'react';
-import {
-    Card,
-    CardBody,
-    CardFooter,
-  } from "reactstrap";
+import React from "react";
+import { Card, CardBody, CardFooter } from "reactstrap";
 
 const Band = (props) => {
-    return (
-        <Card className="card-user max30w simple-margin">
-            <div className="image">
-                <img
-                alt={props.image}
-                src={require("assets/img/damir-bosnjak.jpg").default}
-                />
+  return (
+    <Card className="card-user max30w simple-margin">
+      <div className="image">
+        <img
+          alt={props.image}
+          src={require("assets/img/bands-image.jpg").default}
+        />
+      </div>
+      <CardBody>
+        <div className="author">
+          <a href="#card" onClick={(e) => e.preventDefault()}>
+            <img alt="..." className="avatar border-gray" src={props.image} />
+          </a>
+          <h5 className="title spaced-orange">{props.name}</h5>
+        </div>
+        <p className="description text-center">{props.description}</p>
+      </CardBody>
+      <CardFooter>
+        <hr />
+        <div className="button-container d-flex even-spacing">
+          {props.genres.map((genre, i) => (
+            <div className="max30w simple-margin overflow-none" key={i}>
+              <button className="button-cstm">{genre}</button>
             </div>
-            <CardBody>
-                <div className="author">
-                <a href="#card" onClick={(e) => e.preventDefault()}>
-                    <img
-                    alt="..."
-                    className="avatar border-gray"
-                    src={props.image}
-                    />
-                </a>
-                <h5 className="title spaced-orange">{props.name}</h5>
-                <p className="description">extra, small space</p>
-                </div>
-                <p className="description text-center">
-                    {props.description}
-                </p>
-            </CardBody>
-            <CardFooter>
-                <hr />
-                <div className="button-container d-flex even-spacing">
-                    {props.genres.map((genre, i)=>(
-                         <div className="max30w simple-margin overflow-none" key={i}>
-                             <button className="button-cstm">{genre}</button>
-                         </div>
-                    ))}
-                </div>
-            </CardFooter>
-        </Card>
-    )
-}
+          ))}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default Band
+export default Band;
