19c6f141c122a023fc67fa5871208ccff454bf18
Viktor Dome <v.dome1991@gmail.com>
2021-10-07 16:00:27 +0200

    [add] conditional display to bands

--- src/views/Bands.js
+++ src/views/Bands.js
@@ -3,39 +3,53 @@ import { useState, useEffect } from 'react';
 import Band from './Band';
 import { ApiRequestHandler } from "ApiRequestHandler";
 import { backendRoutes } from "routes.js";
+import BandProfile from './profile/BandProfile';
 
 
 const Bands = (props) => {
     const [cards, setCards] = useState([]);
     const [error, setError] = useState('');
+    const [componentId, setComponentId] = useState('');
     const requestUrl = backendRoutes.band.all;
 
     useEffect(() => {
       ApiRequestHandler.get(requestUrl, setCards, setError)
     }, [requestUrl]);
 
+    const handleClick = (id) => {
+      setComponentId(id);
+    };
+
   return (
-    <div className="content">
-      {error ? (
-        <div>
-          An error occured while fetching the requested information. Please try
-          again!
-        </div>
+    <>
+      {componentId === "" ? (
+      <div className="content">
+        {error ? (
+          <div>
+            An error occured while fetching the requested information. Please try
+            again!
+          </div>
+        ) : (
+          <div className="d-flex flex-wrap justify-content-start">
+            {cards.map((card) => (
+              <Band
+                key={card.id}
+                id={card.id}
+                image={card.imageUrl}
+                name={card.name}
+                description={card.description}
+                genres={card.genres}
+                onClick={handleClick}
+              />
+            ))}
+          </div>
+        )}
+      </div>
       ) : (
-        <div className="d-flex flex-wrap justify-content-start">
-          {cards.map((card) => (
-            <Band
-              key={card.id}
-              image={card.imageUrl}
-              name={card.name}
-              description={card.description}
-              genres={card.genres}
-            />
-          ))}
-        </div>
+        <BandProfile id={componentId} onChange={setComponentId} />
       )}
-    </div>
-  );
-};
+    </>
+    );
+  };
 
 export default Bands;
